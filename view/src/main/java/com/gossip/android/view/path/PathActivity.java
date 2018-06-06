d9ac9a50154aa1942f96f8b6cd16055ffd70841f
吴明 <>
2018-06-06 23:06:23 +0800

    测试shader

--- view/src/main/java/com/gossip/android/view/path/PathActivity.java
+++ view/src/main/java/com/gossip/android/view/path/PathActivity.java
@@ -3,6 +3,7 @@ package com.gossip.android.view.path;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.gossip.android.view.R;
 
@@ -12,5 +13,10 @@ public class PathActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.path_activity_path);
+    }
+
+    public void onclick(View view) {
+      PathView view1 =  this.findViewById(R.id.pathView);
+        view1.start();
     }
 }
--- view/src/main/java/com/gossip/android/view/path/PathView.java
+++ view/src/main/java/com/gossip/android/view/path/PathView.java
@@ -1,5 +1,7 @@
 package com.gossip.android.view.path;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,13 +10,17 @@ import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import com.gossip.android.view.utils.Logger;
 
 public class PathView extends View {
 
     private Paint paint;
     private Path path;
     private Wave wave;
-    private int progress;
+    private int progress=50;
+    private int dx;
 
     public PathView(Context context) {
         super(context);
@@ -33,10 +39,11 @@ public class PathView extends View {
         paint = new Paint();
         //抗锯齿
         paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.STROKE);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
         paint.setStrokeWidth(4);
         paint.setColor(Color.RED);
         path = new Path();
+        wave=new Wave(20,200);
     }
 
     @Override
@@ -49,14 +56,91 @@ public class PathView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         path.reset();
-        path.moveTo(0, 100);
-        for (int i = 1; i < getWidth() / wave.width / 2 + 2; i++) {
-            path.quadTo(wave.width / 2 * i, 80, 100, 100);
-            path.quadTo(150, 120, 200, 100);
+        int currentY =this.getHeight()*(100-progress)/100;
+        path.moveTo(0+dx, currentY);
+//        for (int i = 1; i < getWidth() / wave.width / 2 + 2; i++) {
+//            int firstX =wave.width*i;
+//            int secondX=firstX+wave.width;
+//            path.quadTo(firstX-wave.width/2, currentY-wave.height,
+//                    firstX, currentY);
+//            path.quadTo(firstX+wave.width/2, currentY+wave.height,
+//                    secondX, currentY);
+//            Logger.printD("PATH","i:"+i+"\ncontrol1:"+wave.width / 2*i+"\ncontrol2:"+wave.width / 2*(i+2));
+//        }
+
+        for (int i=0;i<getWidth()/wave.width+1;i++){
+            int firstX=wave.width*i+wave.width/2+dx;
+            int secondX =firstX +wave.width/2;
+            int controlX = firstX -wave.width/4;
+            int controlX2 = secondX -wave.width/4;
+            int i1 = wave.width*i+wave.width >> 1;
+            Logger.printD("PATH","i:"+i
+                    +"\nfirstX:"+firstX
+                    +"\ni1:"+i1
+                    +"\nsecondX:"+secondX
+                    +"\ncontrolX:"+controlX
+                    +"\ncontrolX2:"+controlX2);
+            path.quadTo(controlX,currentY-20,firstX,currentY);
+            path.quadTo(controlX2,currentY+20,secondX,currentY);
+
         }
         canvas.drawPath(path, paint);
 
+        path.reset();
+        currentY+=100;
+        path.moveTo(0, currentY);
+        path.quadTo(50,currentY-20,100,currentY);
+        path.quadTo(150,currentY+20,200,currentY);
+
+        path.quadTo(250,currentY-20,300,currentY);
+        path.quadTo(350,currentY+20,400,currentY);
+
+        path.close();
+        canvas.drawPath(path, paint);
+
     }
+
+
+    public void start(){
+        ValueAnimator animator = ValueAnimator.ofInt(0,wave.width);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(1000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                dx= (int) animation.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                dx = 0;
+            }
+        });
+
+        //无限重复
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+
+        animator.start();
+    }
+
 
 
     private class Wave {
@@ -65,7 +149,7 @@ public class PathView extends View {
          */
         private int height = 20;
         /**
-         * 一个波纹的宽度
+         * 一个波纹的长度
          */
         private int width = 100;
 
